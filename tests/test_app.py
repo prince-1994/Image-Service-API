9b0e8090238c543790170a454f673111b7974484
prince-1994 <yuvrajsahu007@gmail.com>
2023-12-17 21:36:19 +0100

    fixed tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,21 +8,21 @@ import os
 
 client = TestClient(app)
 logger = AppLogger(__name__)
-logger.info(os.environ['rapid_api_secret'])
+logger.info(os.environ["rapid_api_secret"])
 
 
 def setup():
     """Setup before tests"""
     # nothing to be done
-    os.mkdir('tmp')
-    os.mkdir('tmp/images')
+    os.mkdir("tmp")
+    os.mkdir("tmp/images")
 
 
 def teardown():
     """Restore the state before setup was run"""
     # nothing to be done
-    os.rmdir('tmp/images')
-    os.rmdir('tmp')
+    os.rmdir("tmp/images")
+    os.rmdir("tmp")
 
 
 @pytest.fixture()
@@ -34,18 +34,26 @@ def setup_teardown():
 
 
 def test_edit_api():
-    headers = {"X-RapidAPI-Proxy-Secret": 'test_secret'}
-    response = client.get('/images/edit')
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    response = client.get('/images/edit', headers=headers)
+    headers = {"X-RapidAPI-Proxy-Secret": "test_secret"}
+    wrong_headers = {"X-RapidAPI-Proxy-Secret": "wrong_secret"}
+    response = client.get("/images/edit")
     assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-    response = client.post('/images/edit', headers=headers)
+    response = client.get("/images/edit", headers=wrong_headers)
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+    response = client.get("/images/edit", headers=headers)
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+    response = client.post("/images/edit")
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-    file_path = 'tests/test-image-1-scenery.jpeg'
+    response = client.post("/images/edit", headers=wrong_headers)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    response = client.post("/images/edit", headers=headers)
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    file_path = "tests/test-image-1-scenery.jpeg"
+    response = client.post("/images/edit",
+                           files={"file": open(file_path, "rb")})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    file_path = "tests/test-image-1-scenery.jpeg"
     response = client.post(
-        '/images/edit',
-        headers=headers,
-        files={
-            'file': open(file_path, 'rb')})
-    logger.info(response.content)
+        "/images/edit", headers=headers, files={"file": open(file_path, "rb")}
+    )
     assert response.status_code == status.HTTP_200_OK
